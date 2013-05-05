489fda469bf5502a905647f536c0abc749e08566
matheus23 <matheus.dev@googlemail.com>
2013-05-05 14:14:33 +0200

    Added CollectionSpecs so that Mapping2Specs is executed on major testing.

---
+++ ScalaUtilsTests/src/org/matheusdev/specs/CollectionSpecs.scala
@@ -0,0 +1,14 @@
+package org.matheusdev.specs
+
+import org.scalatest.Specs
+import org.matheusdev.collections.mapping2d.tests.Mapping2Spec
+
+/*
+ * Created with IntelliJ IDEA.
+ * Author: matheusdev
+ * Date: 5/5/13
+ * Time: 2:13 PM
+ */
+class CollectionSpecs extends Specs(
+  new Mapping2Spec
+)
