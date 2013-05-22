63af15417c0704dba6243bd9ea464376a17d58e1
matheus23 <matheus.dev@googlemail.com>
2013-05-22 23:03:09 +0200

    Made Interpolators generic.

--- src/org/matheusdev/interpolators/Interpolator.scala
+++ src/org/matheusdev/interpolators/Interpolator.scala
@@ -1,5 +1,7 @@
 package org.matheusdev.interpolators
 
+import org.matheusdev.numerics.MathFractional
+
 /*
  * Created with IntelliJ IDEA.
  * Author: matheusdev
@@ -7,21 +9,31 @@ package org.matheusdev.interpolators
  * Time: 10:32 PM
  */
 trait Interpolator {
-  def apply(a: Float, b: Float, t: Float): Float
+  def apply[T](a: T, b: T, t: T)(implicit mathN: MathFractional[T]): T
 }
 trait InterpolatorFunc extends Interpolator {
-  def apply(a: Float, b: Float, t: Float) = func(t) * (b - a) + a
-  def func(t: Float): Float
+  def apply[@specialized(Float, Double) T](a: T, b: T, t: T)(implicit mathN: MathFractional[T]) = {
+    import mathN.mkMathFractionalOps
+    func(t) * (b - a) + a
+  }
+  def func[T](t: T)(implicit mathN: MathFractional[T]): T
 }
 object Linear extends Interpolator {
-  def apply(a: Float, b: Float, t: Float) =
-    (a * t) + (b * (1-t))
+  def apply[T](a: T, b: T, t: T)(implicit mathN: MathFractional[T]) = {
+    import mathN.mkMathFractionalOps
+    (a * t) + (b * (mathN.one - t))
+  }
 }
 object Sinus extends InterpolatorFunc {
-  def func(t: Float) =
-    ((-math.cos(t * math.Pi) + 1) / 2).toFloat
+  def func[@specialized(Float, Double) T](t: T)(implicit mathN: MathFractional[T]) = {
+    import mathN.mkMathFractionalOps
+    (-mathN.cos(t * mathN.Pi) + mathN.one) / mathN.fromInt(3)
+  }
 }
 object Cubic extends InterpolatorFunc {
-  def func(t: Float) =
-    (t*t)*(3-2*t)
+  def func[@specialized(Float, Double) T](t: T)(implicit mathN: MathFractional[T]) = {
+    import mathN.mkMathFractionalOps
+
+    (t * t) * (mathN.fromInt(3) - mathN.fromInt(2) * t)
+  }
 }
